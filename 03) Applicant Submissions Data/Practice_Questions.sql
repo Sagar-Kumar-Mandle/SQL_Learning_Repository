be084f84841adcf442d556bf2285f9fbac7c6ec2
Sagar Kumar Mandle <sagarmandle11135@gmail.com>
2026-05-14 21:49:38 +0530

    Add files via upload

---
+++ 03) Applicant Submissions Data/Practice_Questions.sql
@@ -0,0 +1,22 @@
+SELECT * FROM resume
+---------------------------------------------------------------------
+Q1) List All Distinct Users and Their Stats
+--  Return the user name, total submissions, and total points earned by each user.
+--  A list of users with their submission count and total points.
+
+Q2) Calculate the Daily Average Points for Each User
+--  For each day, calculate the average points earned by each user.
+--  A report showing the average points per user for each day.
+
+Q3) Find the Top 3 Users with the Most Correct Submissions for Each Day
+--  Identify the top 3 users with the most correct submissions for each day.
+--  A list of users and their correct submissions, ranked daily.
+
+Q4) Find the Top 5 Users with the Highest Number of Incorrect Submissions.
+--  Identify the top 5 users with the highest number of incorrect submissions.
+--  A list of users with the count of incorrect submissions.
+
+Q5) Find the top 10 performers for each week.
+--  Identify the top 10 users with the highest total points earned each week.
+--  A report showing the top 10 users ranked by total points per week.
+
