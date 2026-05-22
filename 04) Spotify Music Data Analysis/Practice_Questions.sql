1f22f0875c33a00c75027d6f7fab7b313e3041be
Sagar Kumar Mandle <sagarmandle11135@gmail.com>
2026-05-22 18:49:21 +0530

    Add files via upload

---
+++ 04) Spotify Music Data Analysis/Practice_Questions.sql
@@ -0,0 +1,33 @@
+SELECT * FROM spotify;
+------------------------------------------------------------------------------------------
+Q1) Retrieve the names of all tracks that have more than 1 billion streams.
+
+Q2) List all albums along with their respective artists.
+
+Q3) Get the total number of comments for tracks where `licensed = TRUE`.
+
+Q4) Find all tracks that belong to the album type `single`.
+
+Q5) Count the total number of tracks by each artist..
+
+Q6) Calculate the average danceability of tracks in each album.
+
+Q7) Find the top 5 tracks with the highest energy values.
+
+Q8) List all tracks along with their views and likes where `official_video = TRUE`.
+
+Q9) For each album, calculate the total views of all associated tracks.
+
+Q10) Retrieve the track names that have been streamed on Spotify more than YouTube.
+
+Q11) Find the top 3 most-viewed tracks for each artist using window functions.
+
+Q12) Write a query to find tracks where the liveness score is above the average.
+
+Q13) Use a WITH clause to calculate the difference between the highest and lowest energy values for tracks in each album.
+
+Q14) Find tracks where the energy-to-liveness ratio is greater than 1.2.
+
+Q15) Calculate the cumulative sum of likes for tracks ordered by the number of views, using window functions.
+
+
