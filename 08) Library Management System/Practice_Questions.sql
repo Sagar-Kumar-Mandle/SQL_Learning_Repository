b27e813ffe2b9373276a429ea2e6e93ffef2d647
Sagar Kumar Mandle <sagarmandle11135@gmail.com>
2026-06-09 20:00:38 +0530

    Add files via upload

---
+++ 08) Library Management System/Practice_Questions.sql
@@ -0,0 +1,34 @@
+SELECT * FROM branch
+SELECT * FROM employee
+SELECT * FROM members
+SELECT * FROM books
+SELECT * FROM issued_status
+SELECT * FROM return_status
+---------------------------------------------
+Q1)  Create a New Book Record
+   '978-1-60129-456-2', 'To Kill a Mockingbird', 'Classic', 6.00, 'yes', 'Harper Lee', 'J.B. Lippincott & Co.'
+   
+Q2) Update an Existing Members Address
+
+Q3) Delete a Record from the Issued Status Table
+    Objective: Delete the record with issued_id = 'IS121' from the issued_status table.
+
+Q4) Retrieve All Books Issued by a Specific Employee
+     Objective: Select all books issued by the employee with emp_id = 'E101'.
+	 
+Q5) List Members Who Have Issued More Than One Book
+
+Q6) Create Summary Tables**: 
+    Used CTAS to generate new tables based on query results - each book and total book_issued
+
+Q7) Retrieve All Books in a Classic Category
+
+Q8) Find Total Rental Income by Category:
+
+Q9) List Employees with Their Branch Managers Name and their branch details 
+
+Q10) Create a Table of Books with Rental Price Above a Certain Threshold
+
+Q11) Retrieve the List of Books Not Yet Returned
+
+
