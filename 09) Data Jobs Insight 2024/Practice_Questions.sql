bc1a03fdae97878bdbaef921deadbc74d5742535
Sagar Kumar Mandle <sagarmandle11135@gmail.com>
2026-06-13 22:24:45 +0530

    Add files via upload

---
+++ 09) Data Jobs Insight 2024/Practice_Questions.sql
@@ -0,0 +1,27 @@
+SELECT * FROM company_dim;
+SELECT * FROM job_postings_fact;
+SELECT * FROM skill_dim;
+SELECT * FROM skill_job_dim;
+------------------------------------------------------------------------
+Q1) Retrieve the top 10 highest-paid Data Analyst jobs.
+   Exclude records with NULL salary values.
+   Only include jobs where the location is 'Anywhere'.
+   
+Q2) Retrieve the top 10 highest-paid Data Analyst jobs.
+   Exclude records with NULL salary values.
+   Only include jobs where the location is 'Anywhere'.
+   What skill is required for the post.
+   
+Q3) What are top 5 most demand skill for Data Analyst role
+
+Q4) What are the top 10 highest-paying job postings for the 'Data Analyst' role that require SQL skills?
+
+Q5) Most demand and highest paying skill for Data Analyst
+
+Q6) How many Data Analyst job postings were made in each month?
+
+Q7) Added a new column based on salary.find all location _category for data anlayst
+
+Q8) Find the company has most data anlayst job opening, total number oj job(per customner_id) return with customer_name
+
+
